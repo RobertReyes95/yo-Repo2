f712d690d277bfba964337b9c41b3a8cd130b21a
Robert Reyes <Robert.Reyes@elma.com>
2021-02-18 11:58:38 -0800

    testing re module

--- pythonPractice.py
+++ pythonPractice.py
@@ -1,3 +1,12 @@
 import os
 import sys
+import argparse
+import re 
 
+foot = re.findall(r'\bf[a-z]*', 'which foot or hand fell fastest')
+print(foot)
+
+ #Testing out the re module,  this takes out duplicates in a sentence
+sentence = input('Please enter your sentence here: ')
+takeDuplicate = re.sub(r'(\b[a-z]+) \1', r'\1', sentence)
+print(takeDuplicate)
